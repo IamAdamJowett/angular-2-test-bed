803e5622c052d0bd3bb39dfbe89692d129619d71
Adam Jowett <akuma.me@gmail.com>
2017-01-28 18:20:59 +1000

    Removed shared module from about module

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -3,14 +3,12 @@ import { CommonModule } from '@angular/common';
 import { AboutRoutingModule } from './about-routing.module';
 import { AboutComponent } from './about.component';
 
-import { SharedModule } from '../shared/shared.module';
-
 @NgModule({
   imports: [
     CommonModule,
     AboutRoutingModule,
-    SharedModule
   ],
   declarations: [AboutComponent]
 })
+
 export class AboutModule { }
